0be3d8bf25e0e0d60da501533fb7c666765d2b1c
shital bramhe <shitalbramhe@gmail.com>
2022-01-20 21:18:46 +0530

    [Shital] add : Validate Mobile number using regex pattern

--- javascript/InpuForm.js
+++ javascript/InpuForm.js
@@ -36,3 +36,18 @@ email.addEventListener('input', function()
 
         }
     });
+
+    //uc3-validating mobile number
+const tel = document.querySelector('#tel');
+const telError = document.querySelector('.tel-error');
+tel.addEventListener('input', function () {
+    let telRegex = RegExp('^[0-9]{2}[ ][0-9]{10}$');
+    if (telRegex.test(tel.value)) 
+    {
+        telError.textContent = "";
+    }
+    else 
+    {
+        telError.textContent = "Moble number is not Valid";
+    }
+});
